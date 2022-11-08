9ca235a69f2f65b8e0701e4f6ed963b2dc485692
karolisjon <karolisjonn@gmail.com>
2022-11-08 21:18:29 +0200

    refactor: created a separate component for CardHeader

---
+++ src/components/card-header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/UI/card-header.css';
+
+const CardHeader = () => {
+  const date = new Date();
+  const day = date.toLocaleString('en-US', { day: 'numeric' });
+  const weekday = date.toLocaleString('en-US', { weekday: 'long' }).toUpperCase();
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const year = date.getFullYear();
+
+  return (
+    <div className='card-header'>
+      <div className='card-header__date-container'>
+        <h3 className='card-header__day'>{day}</h3>
+        <div>
+          <h3>{month}</h3>
+          <h3>{year}</h3>
+        </div>
+      </div>
+      <h3 className='card-header__weekday'>{weekday}</h3>
+    </div>
+  )
+}
+
+export default CardHeader;
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
+import CardHeader from './card-header';
 import '../styles/UI/card.css';
 
 const Card = () => {
-  const date = new Date();
-  const day = date.toLocaleString('en-US', { day: 'numeric' });
-  const weekday = date.toLocaleString('en-US', { weekday: 'long' }).toUpperCase();
-  const month = date.toLocaleString('en-US', { month: 'long' });
-  const year = date.getFullYear();
 
   return (
     <div className='card'>
-      <div className='card-header'>
-        <div className='card-header__date-container'>
-          <h3 className='card-header__day'>{day}</h3>
-          <div>
-            <h3>{month}</h3>
-            <h3>{year}</h3>
-          </div>
-        </div>
-        <h3 className='card-header__weekday'>{weekday}</h3>
-      </div>
+      <CardHeader />
     </div>
   )
 }
