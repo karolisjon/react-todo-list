400dcdf9f24efa68d13165aa177f15a312ea3541
karolisjon <karolisjonn@gmail.com>
2022-11-08 21:32:08 +0200

    feat: initial creation of CardButton

---
+++ src/components/card-button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/UI/card-button.css';
+
+const CardButton = () => {
+  return (
+    <div className='card-button-container'>
+      <button type='button' className='card-button'>+</button>
+    </div>
+  )
+}
+
+export default CardButton;
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CardHeader from './card-header';
 import '../styles/UI/card.css';
+import CardButton from './card-button';
 
 const Card = () => {
 
   return (
     <div className='card'>
       <CardHeader />
+      <CardButton />
     </div>
   )
 }
