8c542d6fdc7c6ae3eb93a360754d698c781e8b6d
karolisjon <karolisjonn@gmail.com>
2022-11-23 18:53:53 +0200

    feat: adjusted TodoItemsContainer component to show an image when no todo items are added

--- src/components/todo-item/todo-items-container/todo-items-container.jsx
+++ src/components/todo-item/todo-items-container/todo-items-container.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import TodoItem from '../todo-item';
+import NoTodoItems from '../../../assets/no-item-found.jpg';
 import './todo-items-container.css';
 
 const TodoItemsContainer = ({ todoItems, onDelete }) => {
 
   return (
     <div className='todo-items-container'>
+      {todoItems.length === 0 &&
+        <div className='empty'>
+          <img src={NoTodoItems} alt="" />
+        </div>}
       {todoItems.map(({ id, text }) => (
-        <TodoItem 
-        key={id} 
-        id={id}
-        text={text}
-        onDelete={onDelete}
+        <TodoItem
+          key={id}
+          id={id}
+          text={text}
+          onDelete={onDelete}
         />
       ))}
     </div>
