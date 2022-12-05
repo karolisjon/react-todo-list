2703043be4a7d2defe2c838a00e39a041857576b
karolisjon <karolisjonn@gmail.com>
2022-12-05 17:24:42 +0200

    feat: created portal div in index.html and used createPortal from ReactDOM in Modal component

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import '../modal/modal.css';
 
 const Modal = ({ open }) => {
   if(!open) return null;
 
-  return (
+  return ReactDOM.createPortal(
     <form action="submit" className='modal'>
       <label for='edit-input'>Edit your todo item</label>
       <div className='input-container'>
         <input type="text" id='edit-input' />
         <button className='edit-button'>Done</button>
       </div>
-    </form>
+    </form>,
+    document.getElementById('portal')
   )
 }
 
